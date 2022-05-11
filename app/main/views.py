1fb65d10f843e51ac4fa0d74c07a0393fee8f33c
FloWambui <gflorencewambui@gmail.com>
2022-05-11 10:58:13 +0300

    add new user profil page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template,request,redirect, url_for,abort
+from flask_login import login_required
 from . import main
-
+from ..models import User
+from .forms import UpdateProfile
+from .. import db
 
 @main.route('/')
 def home():
@@ -8,3 +11,31 @@ def home():
     message = "Make your Best Impression. "
     
     return render_template('index.html',title=title,message=message)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
